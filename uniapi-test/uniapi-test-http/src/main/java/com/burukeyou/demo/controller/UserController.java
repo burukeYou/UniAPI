131335070546ab9e818ab162adf9f89935de1668
caizhihao <caizhihao@163.com>
2024-12-28 10:37:49 +0800

    refactor():  remove Useless

--- uniapi-test/uniapi-test-http/src/main/java/com/burukeyou/demo/controller/UserController.java
+++ uniapi-test/uniapi-test-http/src/main/java/com/burukeyou/demo/controller/UserController.java
@@ -1,14 +1,5 @@
 package com.burukeyou.demo.controller;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.alibaba.fastjson2.JSON;
 import com.burukeyou.demo.entity.Add4DTO;
 import com.burukeyou.demo.entity.BaseRsp;
@@ -17,19 +8,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("/user-web")
@@ -98,6 +87,7 @@ public class UserController {
     // Content-Type:  application/octet-stream
     //  consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE
     @PostMapping(value = "/add7")
+    @ResponseBody
     public BaseRsp<String> add7(@RequestBody byte[] data) throws IOException {
         log.info("收到请求7- dataSize:{}",data.length);
         return BaseRsp.ok("1");
